062dbc3b9268759be86480594588953b3b0f23c7
Nara <narawit.tub@outlook.co.th>
2021-10-22 19:57:57 +0700

    Add project files.

---
+++ LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HRLeaveManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            // Note: We don't do this because we need to write the line like this for every MappingProfile we have,
+            // assuming we gonna have multiple MappingProfile
+            // services.AddAutoMapper(typeof(MappingProfile));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
