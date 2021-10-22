e2ce45dd6173eac9f3ec5500cf29c3f3f1c15e99
Nara <narawit.tub@outlook.co.th>
2021-10-23 00:42:50 +0700

    feat: setting mediatorR on LeaveType

--- LeaveManagement.Application/ApplicationServiceRegistration.cs
+++ LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace HRLeaveManagement.Application
 {
     public static class ApplicationServiceRegistration
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
             // Note: We don't do this because we need to write the line like this for every MappingProfile we have,
             // assuming we gonna have multiple MappingProfile
             // services.AddAutoMapper(typeof(MappingProfile));
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
         }
     }
 }
